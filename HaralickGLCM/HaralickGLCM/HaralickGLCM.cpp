130d1f7ef7cc5fcb1096f8209e543c53decf208f
marcinkociolek <marcin.kociolek@gmail.com>
2017-02-01 18:45:31 +0100

    Incremental

--- HaralickGLCM/HaralickGLCM/HaralickGLCM.cpp
+++ HaralickGLCM/HaralickGLCM/HaralickGLCM.cpp
@@ -37,8 +37,10 @@ using namespace boost::filesystem;
 int main(int argc, char* argv[])
 {
 	bool displayInput = true;
-	path PathToProcess("C:\\Data\\FeatureVariabilityTest\\In");
-	path PathROI("C:\\Data\\FeatureVariabilityTest\\InData\\ROIs512x512c51x51xCount181");
+	path PathToProcess("C:\\Data\\FeatureVariabilityTest\\InData\\BarsA0-90F08T04N00000");
+	path PathROIFile("C:\\Data\\FeatureVariabilityTest\\InData\\ROIs512x512c51x51xCount181\\ROIRectSize51x51Nr000.tif");
+
+	regex FilePattern(".+.tif");
 	if (!exists(PathToProcess))
 	{
 		cout << PathToProcess << " not exists " << '\n';
@@ -60,12 +62,110 @@ int main(int argc, char* argv[])
 		//return 0;
 	}
 
+	if (!exists(PathROIFile))
+	{
+		cout << PathToProcess << " not exists " << '\n';
+		return 0;
+	}
+	Mat ImRoi = imread(PathROIFile.string(), CV_LOAD_IMAGE_ANYDEPTH);
+	
+	if (!ImRoi.cols || !ImRoi.rows)
+	{
+		cout << "No vailid Roi file";
+		return 0;
+	}
+
+	int *maxRoiX = new int[65536];
+	int *minRoiX = new int[65536];
+	int *maxRoiY = new int[65536];
+	int *minRoiY = new int[65536];
+
+	int maxRoiNr = 0;
+
+	for (int i = 0; i < 65536; i++)
+	{
+		maxRoiX[i] = 0;
+		minRoiX[i] = 65536;
+		maxRoiY[i] = 0;
+		minRoiY[i] = 65536;
+	}
+
+	if (ImRoi.depth() != CV_16U)
+		ImRoi.convertTo(ImRoi, CV_16U);
+	unsigned short *wImRoi = (unsigned short *)ImRoi.data;
+	for (int y = 0; y < ImRoi.rows; y++)
+	{
+		for (int x = 0; x < ImRoi.cols; x++)
+		{
+			if (*wImRoi)
+			{
+				if (maxRoiX[*wImRoi] < x)
+					maxRoiX[*wImRoi] = x;
+				if (minRoiX[*wImRoi] > x)
+					minRoiX[*wImRoi] = x;
+				if (maxRoiY[*wImRoi] < y)
+					maxRoiY[*wImRoi] = y;
+				if (minRoiY[*wImRoi] > y)
+					minRoiY[*wImRoi] = y;
+
+				if (maxRoiNr < *wImRoi)
+					maxRoiNr = *wImRoi;
+			}
+			wImRoi++;
+		}
+	}
+	
+	cout << "Roi file  - " << PathROIFile.filename().string() << "\n";
+	
 
 	if (displayInput)
 	{
 		namedWindow("Image", WINDOW_AUTOSIZE);
+		namedWindow("ROI", WINDOW_AUTOSIZE);
 	}
-	
+
+	string OutString;
+	for (directory_entry& FileToProcess : directory_iterator(PathToProcess))
+	{
+		path InPath = FileToProcess.path();
+
+		//string OutString = ProcOptionsStr;
+
+
+		if (!regex_match(InPath.filename().string().c_str(), FilePattern))
+			continue;
+
+		if (!exists(InPath))
+		{
+			cout << InPath.filename().string() << " File not exists" << "\n";
+			continue;
+		}
+
+		OutString += "In file - " + InPath.filename().string() + "\n";
+		cout << "In file  - " << InPath.filename().string() << "\n";
+
+		Mat ImIn = imread(InPath.string(), CV_LOAD_IMAGE_ANYDEPTH);
+
+		if (!ImIn.size)
+		{
+			cout << "this is not a valid image file";
+			continue;
+		}
+
+		int maxX, maxY, maxXY;
+		maxX = ImIn.cols;
+		maxY = ImIn.rows;
+		maxXY = maxX * maxY;
+
+
+		if (displayInput)
+		{
+			Mat ImRGB = ShowImage16PseudoColor(ImIn, 0.0, 65535.0);
+			imshow("Image", ImRGB);
+			imshow("ROI", ShowSolidRegionOnImage(ImRoi, ImRGB));
+			waitKey();
+		}
+	}
 	return 0;
 }
 
